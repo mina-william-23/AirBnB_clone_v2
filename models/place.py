3fb8e64add6a6574029d1b06e1fe3aaf671e2e15
mina-william-23 <capeandroid50@gmail.com>
2024-01-17 19:04:05 +0200

    edit style

--- models/place.py
+++ models/place.py
@@ -6,14 +6,14 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.testing.schema import Table
 
-
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'),
-           primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'),
-           primary_key=True, nullable=False),
-)
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table(
+        'place_amenity', Base.metadata,
+        Column('place_id', String(60),
+               ForeignKey('places.id'), primary_key=True, nullable=False),
+        Column('amenity_id', String(60),
+               ForeignKey('amenities.id'), primary_key=True, nullable=False),
+    )
 
 
 class Place(BaseModel, Base):
