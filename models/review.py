3627b8afa15a429649bf8419d498a0b77edb4074
mina-william-23 <capeandroid50@gmail.com>
2024-01-17 16:48:24 +0200

    task 9

--- models/review.py
+++ models/review.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
+import os
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
