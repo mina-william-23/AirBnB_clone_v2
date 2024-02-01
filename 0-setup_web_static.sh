3ab2f4ad2fe56cfcfb8a87c7ad4a1613e9d8e964
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-02 01:23:44 +0200

    Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get -y update
-sudo apt-get -y install nginx
+apt-get -y update
+apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Hello world!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
