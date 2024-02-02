34aa81b0f670db85e4748d3afd3261c271c9f796
mina-william-23 <capeandroid50@gmail.com>
2024-02-02 02:42:45 +0200

    test 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Fabric script that generates a .tgz archive'''
+from datetime import datetime
+from fabric.api import local, task
+
+
+@task
+def do_pack():
+    '''Function that converts a .tgz archive'''
+
+    local("mkdir -p versions", capture=True)  # capture=True to hide the output
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(date)
+    try:
+        # compress the file in path to web_static
+        local("tar -cvzf {} web_static".format(file_path))
+        return file_path
+    except Exception as e:
+        return None
