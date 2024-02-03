8427b53f1f589dd614e6cf8ebf02d99825ad552b
mina-william-23 <capeandroid50@gmail.com>
2024-02-03 02:28:45 +0200

    test 11

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """ Fabric module """
-from fabric.api import env, put, run
+from fabric.api import env, put, run, task
 import os
 env.hosts = ['18.207.112.242', '54.167.84.94']
 
 
+@task
 def do_deploy(archive_path):
     """Distribute an Archive to servers"""
     if not os.path.exists(archive_path):
         return False
     try:
         file_name = archive_path.split("/")[-1].split(".")[0]
-
         put(archive_path, '/tmp/')
-        # ! just for testing
-        # //run('rm -rf /data/web_static/releases/*')
         run('mkdir /data/web_static/releases/{}'.format(file_name))
         run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.
             format(file_name, file_name))
@@ -26,6 +24,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.
             format(file_name))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
