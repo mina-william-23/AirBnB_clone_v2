d50cd879bada3a188aa218908966a02e2093baef
mina-william-23 <capeandroid50@gmail.com>
2024-02-03 03:36:30 +0200

    test 20

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,6 +2,8 @@
 """Fabric script that generates a .tgz archive from the contents of the"""
 from fabric.api import sudo, env, put, local, task
 from datetime import datetime
+import os
+env.hosts = ['18.207.112.242', '54.167.84.94']
 
 
 @task
@@ -18,20 +20,6 @@ def do_pack():
         return None
 
 
-def get_ip_address(domain):
-    """Function To Get IP Address"""
-    import socket
-    try:
-        ip_address = socket.gethostbyname(domain)
-        return ip_address
-    except socket.gaierror:
-        return False
-
-
-env.hosts = [get_ip_address("web-01.minawilliam.tech"),
-             get_ip_address("web-02.minawilliam.tech")]
-
-
 @task
 def do_deploy(archive_path):
     """Function To Deploy File"""
@@ -40,32 +28,23 @@ def do_deploy(archive_path):
     do_deploy:archive_path=versions/web_static_20231009012456.tgz
     -i ./alx -u root
     """
-    import os
     if not os.path.exists(archive_path):
         return False
     try:
-        put(archive_path, "/tmp/")
-
-        folder_to_save = "/data/web_static/releases"
-        file_name_generated = archive_path.split(".")[0]
-        file_name_generated = file_name_generated.split("/")[-1]
-
-        server_archive_path = f"/tmp/{file_name_generated}.tgz"
-        sudo(f"mkdir -p {folder_to_save}/{file_name_generated}")
-        sudo(f"tar -xzf /tmp/{file_name_generated}.tgz "
-             f"-C {folder_to_save}/{file_name_generated}")
-
-        sudo(f"rm {server_archive_path}")
-        sudo(f"mv {folder_to_save}/{file_name_generated}/web_static/*"
-             f" {folder_to_save}/{file_name_generated}/")
-        sudo(f"rm -rf {folder_to_save}/{file_name_generated}/web_static")
-
-        try:
-            sudo('rm -rf /data/web_static/current')
-        except BaseException:
-            pass
-        sudo(f"ln -s {folder_to_save}/{file_name_generated}"
-             f" /data/web_static/current")
+        file_name = archive_path.split("/")[-1].split(".")[0]
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(file_name))
+        sudo('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.
+             format(file_name, file_name))
+        sudo('mv /data/web_static/releases/{}/web_static/* \
+             /data/web_static/releases/{}/'.
+             format(file_name, file_name))
+        sudo('rm -rf /data/web_static/releases/{}/web_static'.
+             format(file_name))
+        sudo('rm -rf /tmp/{}.tgz'.format(file_name))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.
+             format(file_name))
         print("New version deployed!")
         return True
     except Exception:
