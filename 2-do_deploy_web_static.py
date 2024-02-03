aa992049080ead1cf4475b689d0666e6c2491f55
mina-william-23 <capeandroid50@gmail.com>
2024-02-03 03:21:59 +0200

    test 17

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,23 +18,20 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/")
 
-        folder_to_save = "/data/web_static/releases"
-        file_name = archive_path.split("/")[-1].split(".")[0]
-        server_archive_path = f"/tmp/{file_name}.tgz"
+        folder = "/data/web_static/releases"
+        file = archive_path.split("/")[-1].split(".")[0]
 
-        run("mkdir -p {}/{}".format(folder_to_save, file_name))
-        run("tar -xzf /tmp/{}.tgz -C {}/{}".
-            format(file_name, folder_to_save, file_name))
-
-        run("rm {}".format(server_archive_path))
+        run("mkdir -p {}/{}".format(folder, file))
+        run("tar -xzf /tmp/{}.tgz -C {}/{}"
+            format(file, folder, file))
+        run('rm -rf /tmp/{}.tgz'.format(file))
         run('mv {}/{}/web_static/* {}/{}/'.
-            format(folder_to_save, file_name, folder_to_save, file_name))
-        run("rm -rf {}/{}/web_static".format(folder_to_save, file_name))
+            format(folder, file, folder, file))
+        run("rm -rf {}/{}/web_static".format(folder, file))
 
         run('rm -rf /data/web_static/current')
 
-        run('ln -sf {}/{} /data/web_static/current'.
-            format(folder_to_save, file_name))
+        run('ln -sf {}/{} /data/web_static/current'.sformat(folder, file))
         print("New version deployed!")
         return True
     except Exception:
