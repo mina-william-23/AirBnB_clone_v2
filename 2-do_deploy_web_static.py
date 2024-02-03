56d775ed1360b85b883b2ea1f023c0d37da14111
mina-william-23 <capeandroid50@gmail.com>
2024-02-03 02:04:53 +0200

    test 5

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,8 @@
 """Fabric script that generates a .tgz archive from the contents of the"""
 # from fabric.api import sudo, env, put, local, task
 from fabric.api import *
-from fabric.operations import put, sudo
+from fabric.operations import put, run
+import os
 
 
 env.hosts = ['18.207.112.242', '54.167.84.94']
@@ -15,35 +16,24 @@ def do_deploy(archive_path):
     do_deploy:archive_path=versions/web_static_20231009012456.tgz
     -i ./alx -u root
     """
-    import os
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-
-        folder_to_save = "/data/web_static/releases"
-        file_name_generated = archive_path.split(".")[0]
-        file_name_generated = file_name_generated.split("/")[-1]
-
-        server_archive_path = "/tmp/{}.tgz".format(file_name_generated)
-        sudo("mkdir -p {}/{}")
-        sudo("tar -xzf /tmp/{}.tgz -C {}/{}".
-             format(file_name_generated, folder_to_save, file_name_generated))
-
-        sudo("rm {}".format(server_archive_path))
-        sudo("mv {}/{}/web_static/* {}/{}/".
-             format(folder_to_save, file_name_generated,
-                    folder_to_save, file_name_generated))
-        sudo("rm -rf {}/{}/web_static".
-             format(folder_to_save, file_name_generated))
-
-        try:
-            sudo('rm -rf /data/web_static/current')
-        except BaseException:
-            pass
-        sudo("ln -s {}/{} /data/web_static/current".
-             format(folder_to_save, file_name_generated))
-        print("New version deployed!")
+        archive = archive_path.split("/")[-1]
+        path = "/data/web_static/releases"
+        put("{}".format(archive_path), "/tmp/{}".format(archive))
+        folder = archive.split(".")
+        run("mkdir -p {}/{}/".format(path, folder[0]))
+        new_archive = '.'.join(folder)
+        run("tar -xzf /tmp/{} -C {}/{}/"
+            .format(new_archive, path, folder[0]))
+        run("rm /tmp/{}".format(archive))
+        run("mv {}/{}/web_static/* {}/{}/"
+            .format(path, folder[0], path, folder[0]))
+        run("rm -rf {}/{}/web_static".format(path, folder[0]))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {}/{} /data/web_static/current"
+            .format(path, folder[0]))
         return True
-    except Exception:
+    except Exception as e:
         return False
