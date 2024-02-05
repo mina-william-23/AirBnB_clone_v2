72fa3886c832bb5d07682a1c449945acd7fc763f
mina-william-23 <capeandroid50@gmail.com>
2024-02-05 04:28:53 +0200

    test 1000

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,70 +9,53 @@ env.hosts = ['18.207.112.242', '54.167.84.94']
 @task
 def do_pack():
     """
-    making an archive on web_static folder
+        generates a .tgz archine from contents of web_static
     """
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/web_static_{}.tgz".format(time)
+    try:
+        local("mkdir -p ./versions")
+        local("tar --create --verbose -z --file={} ./web_static"
+              .format(file_name))
+        return file_name
+    except Exception as e:
         return None
-    return file
 
 
 @task
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
+    """Distribute an Archive to servers"""
+    if not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    try:
+        file_name = archive_path.split("/")[-1].split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        put(archive_path, '/tmp/')
+        # ! just for testing
+        # //run('rm -rf /data/web_static/releases/*')
+        run('mkdir /data/web_static/releases/{}'.format(file_name))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.
+            format(file_name, file_name))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.
+            format(file_name, file_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(file_name))
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+        run('rm -rf /data/web_static/current')
+        run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.
+            format(file_name))
+        return True
+    except Exception as e:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
 
 
 @task
 def deploy():
-    """
-    Create and distribute an archive to a web server
-    """
-    file = do_pack()
-    if file is None:
+    """full deployment"""
+    try:
+        path = do_pack()
+        if path is None:
+            return False
+        return do_deploy(path)
+    except Exception:
         return False
-    return do_deploy(file)
